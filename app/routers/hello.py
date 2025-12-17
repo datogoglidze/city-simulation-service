cb5b4a3ed678df6b1c03e0833f55abc3f7d20506
Schwifter <d.goglidze@sangu.edu.ge>
2025-12-18 02:13:45 +0400

    Add Simple simulation endpoints

--- app/routers/hello.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter(prefix="/hello", tags=["hellos"])
-
-
-@router.get("/")
-def say_hello() -> str:
-    return "Hello World!"
--- app/runner/fastapi.py
+++ app/runner/fastapi.py
@@ -1,8 +1,24 @@
+import asyncio
+from contextlib import asynccontextmanager
 from dataclasses import dataclass
+from pathlib import Path
+from typing import AsyncGenerator
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
-from app.routers import hello
+from app.routers import simulation
+from app.runner.websocket import ConnectionManager
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    print("Simulation starting...")
+    task = asyncio.create_task(simulation.run_simulation(app.state.manager))
+    yield
+
+    print("Simulation stopping...")
+    task.cancel()
 
 
 @dataclass
@@ -16,8 +32,17 @@ class FastApiConfig:
             title=self.title,
             description=self.description,
             version=self.version,
+            lifespan=lifespan,
         )
 
-        app.include_router(hello.router)
+        app.state.manager = ConnectionManager()
+
+        app.include_router(simulation.router)
+
+        static_dir = Path(__file__).parent.parent.parent / "static"
+        if static_dir.exists():
+            app.mount(
+                "/", StaticFiles(directory=str(static_dir), html=True), name="static"
+            )
 
         return app
