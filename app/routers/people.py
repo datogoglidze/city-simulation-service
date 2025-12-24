310ecd78c8c873ef7fdc3769715852c57016f72f
David Goglidze <d.goglidze@sangu.edu.ge>
2025-12-24 23:11:50 +0400

    Specify responses

--- app/routers/people.py
+++ app/routers/people.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
+from starlette import status
 
 from app.models.person import Location, Person
 from app.runner.dependencies import get_people_service
@@ -17,14 +18,27 @@ class PersonCreate(BaseModel):
     location: PersonLocation
 
 
-@router.get("")
+class PersonRead(BaseModel):
+    id: str
+    location: PersonLocation
+
+
+@router.get(
+    "",
+    status_code=status.HTTP_200_OK,
+    response_model=list[PersonRead],
+)
 def read_all(
     people: PeopleService = Depends(get_people_service),
 ) -> list[Person]:
     return people.read_all()
 
 
-@router.get("/{person_id}")
+@router.get(
+    "/{person_id}",
+    status_code=status.HTTP_200_OK,
+    response_model=PersonRead,
+)
 def read_one(
     person_id: str, people: PeopleService = Depends(get_people_service)
 ) -> Person:
@@ -35,7 +49,11 @@ def read_one(
     return person
 
 
-@router.post("")
+@router.post(
+    "",
+    status_code=status.HTTP_201_CREATED,
+    response_model=PersonRead,
+)
 def create_one(
     person: PersonCreate, people: PeopleService = Depends(get_people_service)
 ) -> Person:
@@ -44,7 +62,11 @@ def create_one(
     return people.create_one(_person)
 
 
-@router.delete("/{person_id}")
+@router.delete(
+    "/{person_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    response_model=None,
+)
 def delete_one(
     person_id: str, people: PeopleService = Depends(get_people_service)
 ) -> None:
