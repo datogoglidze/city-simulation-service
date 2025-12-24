56d1f360fef026a94d3c4bcffd2d5573b4a5caf8
David Goglidze <70262653+datogoglidze@users.noreply.github.com>
2025-12-24 23:40:30 +0400

    People endpoint (#10)

--- app/routers/people.py
+++ app/routers/people.py
@@ -1,14 +1,103 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
+from starlette import status
 
-from app.models.person import Person
+from app.models.person import Location, Person
 from app.runner.dependencies import get_people_service
 from app.services.people import PeopleService
 
 router = APIRouter(prefix="/people", tags=["People"])
 
 
-@router.get("/")
+class PersonLocation(BaseModel):
+    x: int
+    y: int
+
+
+class PersonCreate(BaseModel):
+    location: PersonLocation
+
+
+class PersonRead(BaseModel):
+    id: str
+    location: PersonLocation
+
+
+@router.get(
+    "",
+    status_code=status.HTTP_200_OK,
+    response_model=list[PersonRead],
+)
 def read_all(
     people: PeopleService = Depends(get_people_service),
-) -> list[Person]:
-    return people.read_all()
+) -> list[PersonRead]:
+    _people = people.read_all()
+
+    return [
+        PersonRead(
+            id=person.id,
+            location=PersonLocation(
+                x=person.location.x,
+                y=person.location.y,
+            ),
+        )
+        for person in _people
+    ]
+
+
+@router.get(
+    "/{person_id}",
+    status_code=status.HTTP_200_OK,
+    response_model=PersonRead,
+)
+def read_one(
+    person_id: str, people: PeopleService = Depends(get_people_service)
+) -> PersonRead:
+    _person = people.read_one(person_id)
+    if _person is None:
+        raise HTTPException(status_code=404, detail="Person not found")
+
+    return PersonRead(
+        id=_person.id,
+        location=PersonLocation(
+            x=_person.location.x,
+            y=_person.location.y,
+        ),
+    )
+
+
+@router.post(
+    "",
+    status_code=status.HTTP_201_CREATED,
+    response_model=PersonRead,
+)
+def create_one(
+    person: PersonCreate, people: PeopleService = Depends(get_people_service)
+) -> PersonRead:
+    _person = Person(
+        location=Location(
+            x=person.location.x,
+            y=person.location.y,
+        )
+    )
+
+    created = people.create_one(_person)
+
+    return PersonRead(
+        id=created.id,
+        location=PersonLocation(
+            x=created.location.x,
+            y=created.location.y,
+        ),
+    )
+
+
+@router.delete(
+    "/{person_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    response_model=None,
+)
+def delete_one(
+    person_id: str, people: PeopleService = Depends(get_people_service)
+) -> None:
+    people.delete_one(person_id)
