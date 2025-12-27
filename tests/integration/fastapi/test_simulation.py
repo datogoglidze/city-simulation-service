4beb7150eb9a3143e360b1cffb6a180842a1a510
Schwifter <d.goglidze@sangu.edu.ge>
2025-12-27 22:14:25 +0400

    Add tests

---
+++ tests/integration/fastapi/test_simulation.py
@@ -0,0 +1,39 @@
+import asyncio
+from unittest.mock import ANY
+
+from starlette.testclient import TestClient
+
+
+def test_should_broadcast_person_via_websocket(client: TestClient) -> None:
+    client.post("/people", json={"location": {"x": 0, "y": 0}})
+    client.post("/people", json={"location": {"x": 1, "y": 1}})
+
+    with client.websocket_connect("/simulation/ws") as websocket:
+        asyncio.run(client.app.state.simulation.broadcast_state())
+
+        data = websocket.receive_json()
+
+        assert len(data) == 2
+        assert data == [
+            {"id": ANY, "location": {"x": 0, "y": 0}},
+            {"id": ANY, "location": {"x": 1, "y": 1}},
+        ]
+
+
+def test_should_broadcast_updated_locations(client: TestClient) -> None:
+    client.post("/people", json={"location": {"x": 0, "y": 0}})
+
+    with client.websocket_connect("/simulation/ws") as websocket:
+        asyncio.run(client.app.state.simulation.broadcast_state())
+
+        first_data = websocket.receive_json()
+        assert len(first_data) == 1
+        assert first_data == [{"id": ANY, "location": {"x": 0, "y": 0}}]
+
+        client.app.state.people.update_location()
+
+        asyncio.run(client.app.state.simulation.broadcast_state())
+
+        second_data = websocket.receive_json()
+        assert len(second_data) == 1
+        assert second_data != first_data
