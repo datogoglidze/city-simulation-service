19ced17c7777b2c4a796b658a1735ce0421e4d26
Schwifter <d.goglidze@sangu.edu.ge>
2025-12-30 17:16:16 +0400

    Refactor app initialization

--- app/runner/cli.py
+++ app/runner/cli.py
@@ -1,8 +1,10 @@
+import random
 from pathlib import Path
 
 import uvicorn
 from typer import Typer
 
+from app.models.person import Location, Person
 from app.repositories.in_memory.people import PeopleInMemoryRepository
 from app.repositories.text_file.people_snapshot import PeopleSnapshotJsonRepository
 from app.runner.config import config
@@ -17,15 +19,17 @@ cli = Typer(no_args_is_help=True, add_completion=False)
 @cli.command()
 def run(host: str = "0.0.0.0", port: int = 8000, root_path: str = "") -> None:
     websocket_manager = WebSocketManager()
+
     snapshot_repository = PeopleSnapshotJsonRepository(
         snapshot_file=Path(config.SNAPSHOT_PATH)
     )
+
     people_service = PeopleService(
         people=PeopleInMemoryRepository(),
-        snapshot=snapshot_repository,
         grid_size=config.GRID_SIZE,
-        people_amount=config.PEOPLE_AMOUNT,
     )
+
+    initialize_people(people=people_service, snapshot=snapshot_repository)
 
     uvicorn.run(
         app=FastApiConfig(
@@ -42,3 +46,24 @@ def run(host: str = "0.0.0.0", port: int = 8000, root_path: str = "") -> None:
         port=port,
         root_path=root_path,
     )
+
+
+def initialize_people(
+    people: PeopleService,
+    snapshot: PeopleSnapshotJsonRepository,
+) -> None:
+    initial_people = snapshot.load()
+
+    if not initial_people:
+        initial_people = [
+            Person(
+                location=Location(
+                    x=random.randint(0, config.GRID_SIZE - 1),
+                    y=random.randint(0, config.GRID_SIZE - 1),
+                )
+            )
+            for _ in range(config.PEOPLE_AMOUNT)
+        ]
+
+    for person in initial_people:
+        people.create_one(person)
