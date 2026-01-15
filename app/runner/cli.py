eb13a8477f2948ce18e94006ec84c44702950ec3
David Goglidze <70262653+datogoglidze@users.noreply.github.com>
2026-01-15 17:21:34 +0400

    Extract Uvicorn builder (#35)

--- app/runner/cli.py
+++ app/runner/cli.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import random
 from dataclasses import dataclass
 
-import uvicorn
 from typer import Typer
 
 from app.models.person import Location, Person
@@ -11,7 +10,7 @@ from app.repositories.in_memory.people import PeopleInMemoryRepository
 from app.routers import people, simulation
 from app.runner.config import config
 from app.runner.factory import SnapshotFactory
-from app.runner.fastapi import CityApi
+from app.runner.fastapi import CityApi, UvicornServer
 from app.runner.websocket import WebSocketManager
 from app.services.movement import MovementService
 from app.services.people import PeopleService
@@ -22,7 +21,7 @@ cli = Typer(no_args_is_help=True, add_completion=False)
 
 
 @cli.command()
-def run(host: str = "0.0.0.0", port: int = 8000, root_path: str = "") -> None:
+def run(host: str = "0.0.0.0", port: int = 8000, path: str = "") -> None:
     websocket_manager = WebSocketManager()
 
     people_service = PeopleService(
@@ -36,29 +35,34 @@ def run(host: str = "0.0.0.0", port: int = 8000, root_path: str = "") -> None:
         people_service=people_service,
     )
 
-    PeopleInitializer(
+    people_initializer = PeopleInitializer(
         snapshot_service=snapshot_service,
         grid_size=config.GRID_SIZE,
         people_amount=config.PEOPLE_AMOUNT,
-    ).initialize(people_service)
+        people_service=people_service,
+    )
 
-    uvicorn.run(
-        app=CityApi()
-        .with_router(simulation.router)
-        .with_router(people.router)
-        .with_websocket_manager(websocket_manager)
-        .with_simulation_service(
-            simulation_service=SimulationService(
-                websocket_manager=websocket_manager,
-                people=people_service,
+    (
+        UvicornServer()
+        .with_host(host)
+        .and_port(port)
+        .on_path(path)
+        .before_run(people_initializer.initialize)
+        .run(
+            CityApi()
+            .with_router(simulation.router)
+            .with_router(people.router)
+            .with_websocket_manager(websocket_manager)
+            .with_simulation_service(
+                simulation_service=SimulationService(
+                    websocket_manager=websocket_manager,
+                    people=people_service,
+                )
             )
+            .with_people_service(people_service)
+            .with_snapshot_service(snapshot_service)
+            .build()
         )
-        .with_people_service(people_service)
-        .with_snapshot_service(snapshot_service)
-        .build(),
-        host=host,
-        port=port,
-        root_path=root_path,
     )
 
 
@@ -67,17 +71,18 @@ class PeopleInitializer:
     snapshot_service: SnapshotService | None
     grid_size: int
     people_amount: int
+    people_service: PeopleService
 
-    def initialize(self, people: PeopleService) -> None:
+    def initialize(self) -> None:
         if self.snapshot_service:
             try:
                 self.snapshot_service.load_people()
             except FileNotFoundError:
-                self._generate_people(people)
+                self._generate_people()
         else:
-            self._generate_people(people)
+            self._generate_people()
 
-    def _generate_people(self, people: PeopleService) -> None:
+    def _generate_people(self) -> None:
         max_people = self.grid_size**2
 
         if self.people_amount > max_people:
@@ -90,4 +95,4 @@ class PeopleInitializer:
         ]
 
         for location in random.sample(all_locations, self.people_amount):
-            people.create_one(Person(location=location))
+            self.people_service.create_one(Person(location=location))
