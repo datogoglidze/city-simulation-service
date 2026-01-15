88d7e2f2bc212a1c2153b87a97c90f2513386dc7
Schwifter <d.goglidze@sangu.edu.ge>
2026-01-15 13:06:46 +0400

    Make snapshots optional

--- app/runner/config.py
+++ app/runner/config.py
@@ -8,8 +8,8 @@ load_dotenv()
 class Config:
     GRID_SIZE: int = int(os.getenv("GRID_SIZE", "100"))
     PEOPLE_AMOUNT: int = int(os.getenv("PEOPLE_AMOUNT", "100"))
-    SNAPSHOT_INTERVAL: int = int(os.getenv("SNAPSHOT_INTERVAL", "50"))
-    SNAPSHOT_PATH: str = os.getenv("SNAPSHOT_PATH", "data/people_snapshot.json")
+    SNAPSHOT_PATH: str | None = os.getenv("SNAPSHOT_PATH")
+    SNAPSHOT_INTERVAL: str | None = os.getenv("SNAPSHOT_INTERVAL")
 
 
 config = Config()
