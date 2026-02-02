e8c6329f3f2807a7a81d45c32dea0f12289b6e23
Schwifter <70262653+datogoglidze@users.noreply.github.com>
2026-02-03 00:54:59 +0400

    Rename

--- app/repositories/in_memory/indexes.py
+++ app/repositories/in_memory/indexes.py
@@ -37,11 +37,11 @@ class IndexManager(Generic[TEntity, TId]):
 
     def read_many(self, **filters: Any) -> set[TId]:
         if not filters:
-            raise ValueError("No parameters specified")
+            raise ValueError("No filters specified")
 
         for _field in filters:
             if _field not in self._indexes:
-                raise ValueError(f"Unknown parameter <{_field}>")
+                raise ValueError(f"Unknown filter <{_field}>")
 
         candidate_ids = [
             self._indexes[_field].read_one(value) for _field, value in filters.items()
