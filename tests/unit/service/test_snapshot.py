0acf69400ea74227f826d42a618599c96b76ce18
Schwifter <d.goglidze@sangu.edu.ge>
2026-02-05 00:41:27 +0400

    Test periodic save

--- tests/unit/service/test_snapshot.py
+++ tests/unit/service/test_snapshot.py
@@ -1,3 +1,4 @@
+import asyncio
 from pathlib import Path
 
 import pytest
@@ -132,4 +133,32 @@ def test_should_save_buildings(
 
     assert loaded == [building]
 
+    buildings_snapshot_repository.snapshot_file.unlink()
+
+
+@pytest.mark.anyio
+async def test_should_save_periodically(
+    people_snapshot_repository: PeopleSnapshotJsonRepository,
+    buildings_snapshot_repository: BuildingsSnapshotJsonRepository,
+    people_service: PeopleService,
+    buildings_service: BuildingsService,
+    snapshot_service: SnapshotService,
+) -> None:
+    person = FakePerson().entity
+    building = FakeBuilding().entity
+    people_service.create_one(person)
+    buildings_service.create_one(building)
+    periodic_task = asyncio.create_task(snapshot_service.run_periodic_save())
+
+    await asyncio.sleep(snapshot_service.interval_seconds + 1)
+    periodic_task.cancel()
+    with pytest.raises(asyncio.CancelledError):
+        await periodic_task
+    loaded_person = people_snapshot_repository.load()
+    loaded_building = buildings_snapshot_repository.load()
+
+    assert loaded_person == [person]
+    assert loaded_building == [building]
+
+    people_snapshot_repository.snapshot_file.unlink()
     buildings_snapshot_repository.snapshot_file.unlink()
